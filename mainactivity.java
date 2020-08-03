f7d1af64902b237247779e875aae53ad6b363fae
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-08-03 08:20:44 -0300

    Update mainactivity.java

--- mainactivity.java
+++ mainactivity.java
@@ -1,3 +1,28 @@
+/**
+ *
+ * Creative Commons (CC) 2019 Marcos Vinícius da Silva Santos and Marcos Antonio dos Santos
+ *
+ * Licensed under the Creative Commons, Version 4.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://creativecommons.org/licenses/by-nc-sa/4.0/
+ *    https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *    Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+ *
+ *    https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR
+ *    https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.pt
+ *    Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional (CC BY-NC-SA 4.0)
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
